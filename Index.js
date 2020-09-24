6f72237a23e48cc8aeaa1dee37e416d98db0eb50
Marco Evangelista <marcobjj@users.noreply.github.com>
2020-09-23 17:41:34 -0700

    makes index.js object oriented

--- Index.js
+++ Index.js
@@ -1,5 +1,4 @@
 const app = require('./lib/app')
-const template = require('./src/Template.js');
-const {generatePage, generateCard} = template;
 
-new app(generatePage,generateCard).init();
+
+new app().init();
